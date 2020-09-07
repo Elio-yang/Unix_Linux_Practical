528372d36aa55d590f9db2799b0a849f0222bfa8
Elio-yang <2925539854@qq.com>
2020-09-07 21:39:56 +0800

    cp little fix

--- src/ch2/cp_v1.c
+++ src/ch2/cp_v1.c
@@ -39,7 +39,7 @@
  *   int creat(const char *pathname, mode_t mode);
  *   #include<fcntl.h>
  *   pathname:where is the file
- *   mode:default 0644 -wr-r--r--
+ *   mode:default 0644 (-wr-r--r--)
  * 
  *   ssize_t num_written=write(int fd,void *buf,size_t amt)
  *   #include<unistd.h>
@@ -64,15 +64,18 @@ void oops(const char *s1, const char *s2);
 int main(int argc, char const *argv[])
 {
 	/* decl file descriptor and buffer */
+
 	int src_fd;
 	int dest_fd;
 	char *buf[BUFSIZ];
 	int num_char;
+
 	/* arguments check */
 	if (argc != 3) {
 		fprintf(stderr, "usage: %s source destination \n", argv[0] + 2);
 		exit(LESSARGEXIT);
 	}
+
 	/* open files */
 	if ((src_fd = open(argv[1], O_RDONLY)) == -1) {
 		oops("Can't open ", argv[1]);
@@ -80,6 +83,7 @@ int main(int argc, char const *argv[])
 	if ((dest_fd = creat(argv[2], CREATEMODE)) == -1) {
 		oops("Can't creat", argv[2]);
 	}
+
 	/* write to buffer */
 	while ((num_char = read(src_fd, buf, BUFSIZE)) > 0) {
 		if (write(dest_fd, buf, num_char) != num_char) {
@@ -90,6 +94,7 @@ int main(int argc, char const *argv[])
 	if (num_char == -1) {
 		oops("Read error from ", argv[1]);
 	}
+	
 	/* check close */
 	if (close(src_fd) == -1 || close(dest_fd) == -1) {
 		oops("Error closing file", "");
